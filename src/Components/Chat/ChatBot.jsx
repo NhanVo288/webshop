807e794db64b2b546330af5c1d546c5f58d11339
vnhan28 <vnhan2808@gmail.com>
2026-01-31 20:47:18 +0700

    update

--- src/Components/Chat/ChatBot.jsx
+++ src/Components/Chat/ChatBot.jsx
@@ -37,7 +37,7 @@ const ChatBot = ({ apiKey }) => {
       {
         headers: {
           "Content-Type": "application/json",
-        //   Authorization: `Bearer ${apiKey}`,
+          Authorization: `Bearer`,
         },
       }
     );
